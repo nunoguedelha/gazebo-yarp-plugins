1bd19762b8599c8209535d59164bf640cacba4e1
Nuno Guedelha <nuno.guedelha@iit.it>
2018-07-18 22:49:53 +0200

    Implemented 'GazeboYarpMASwrapper' plugin, which wraps the 'multipleanalogsensorsserver' Yarp device

--- plugins/MASwrapper/include/gazebo/MASwrapper.hh
+++ plugins/MASwrapper/include/gazebo/MASwrapper.hh
@@ -5,8 +5,8 @@
  */
 
 
-#ifndef GAZEBOYARP_MASREMAPPER_HH
-#define GAZEBOYARP_MASREMAPPER_HH
+#ifndef GAZEBOYARP_MASWRAPPER_HH
+#define GAZEBOYARP_MASWRAPPER_HH
 
 #include <gazebo/common/Plugin.hh>
 
@@ -22,27 +22,28 @@ namespace gazebo
         class ImuSensor;
     }
 
-    /// \class GazeboYarpMASremapper
-    /// Gazebo Plugin emulating the "multipleanalogsensorsremapper" device in Gazebo.
+    /// \class GazeboYarpMASwrapper
+    /// Gazebo Plugin emulating the "multipleanalogsensorsserver" device in Gazebo.
     ///
-    /// This plugin instantiates the "multipleanalogsensorsremapper" device for remapping
-    /// the set of inertial sensors accessible through the EMS board(s) which are connected
-    /// (attached) to the MAS remapper device. The remapping is defined by the configuration
+    /// This plugin instantiates the "multipleanalogsensorsserver" device for attaching
+    /// a MAS remapper device or a sensors device 'embObjInertials'. Once the devices are
+    /// attached, the server/wrapper device publishes the sensor data on a YARP port. The
+    /// name of the device to attach as well as the destination port are defined in the
     /// file embedded in the URDF model, which parameters are passed to the Model plugin
     /// calling the methods of this class.
     ///
-    /// The MAS remapper can be configurated using the yarpConfigurationFile sdf
+    /// The MAS wrapper/server can be configurated using the yarpConfigurationFile sdf
     /// tag, that contains a Gazebo URI pointing at a yarp .ini configuration file
-    /// containing the configuration parameters of the remapper.
+    /// containing the configuration parameters of the server.
     ///
     /// The yarpConfigurationFile is the exact same configuration file used on the real
     /// robot for the same purposes.
     ///
-    class GazeboYarpMASremapper : public ModelPlugin
+    class GazeboYarpMASwrapper : public ModelPlugin
     {
     public:
-        GazeboYarpMASremapper();
-        virtual ~GazeboYarpMASremapper();
+        GazeboYarpMASwrapper();
+        virtual ~GazeboYarpMASwrapper();
         /**
          * Saves the gazebo pointer, retrieves the configuration parameters from the
          * sdf file, creates and opens the remapper device with those parameters as input
@@ -51,10 +52,10 @@ namespace gazebo
         virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
 
     private:
-        yarp::dev::PolyDriver m_MASremapperDevice;
+        yarp::dev::PolyDriver m_MASwrapperDevice;
         yarp::dev::IMultipleWrapper* m_iWrap;
         std::string m_robotName;
     };
 }
 
-#endif // GAZEBOYARP_MASREMAPPER_HH
+#endif // GAZEBOYARP_MASWRAPPER_HH
---
+++ plugins/MASwrapper/src/MASwrapper.cc
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2013-2018 Fondazione Istituto Italiano di Tecnologia RBCS & iCub Facility & ADVR
+ * Authors: see AUTHORS file.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or any later version, see LGPL.TXT or LGPL3.TXT
+ */
+
+#include "MASwrapper.hh"
+
+#include <gazebo/physics/physics.hh>
+#include <GazeboYarpPlugins/Handler.hh>
+#include <GazeboYarpPlugins/common.h>
+#include <GazeboYarpPlugins/ConfHelpers.hh>
+
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Log.h>
+#include <yarp/os/LogStream.h>
+
+GZ_REGISTER_MODEL_PLUGIN(gazebo::GazeboYarpMASwrapper)
+
+namespace gazebo {
+
+GazeboYarpMASwrapper::GazeboYarpMASwrapper() : ModelPlugin()
+{
+}
+
+GazeboYarpMASwrapper::~GazeboYarpMASwrapper()
+{
+    if(m_MASwrapperDevice.isValid()) {m_MASwrapperDevice.close();}
+    yarp::os::Network::fini();
+}
+
+void GazeboYarpMASwrapper::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+    yarp::os::Network::init();
+    if (!yarp::os::Network::checkNetwork(GazeboYarpPlugins::yarpNetworkInitializationTimeout))
+    {
+        yError() << "GazeboYarpMASwrapper::Load error: yarp network does not seem to be available, is the yarpserver running?";
+        return;
+    }
+
+    if (!_parent)
+    {
+        gzerr << "GazeboYarpMASwrapper plugin requires a parent Model." << std::endl;
+        return;
+    }
+
+    // Get the model scoped name
+    m_robotName = _parent->GetScopedName(true);
+
+    // The remapper is a YARP device that has already been added in the factory...
+
+    // Getting .ini configuration file parameters from sdf
+    ::yarp::os::Property wrapper_properties;
+
+    bool configuration_loaded = GazeboYarpPlugins::loadConfigModelPlugin(_parent,_sdf,wrapper_properties);
+
+    if (!configuration_loaded)
+    {
+        yError() << "GazeboYarpMASwrapper : File .ini not found, load failed." ;
+        return;
+    }
+
+    // Open the wrapper device
+    if (!m_MASwrapperDevice.open(wrapper_properties))
+    {
+        yError() << "GazeboYarpMASwrapper Plugin Load failed: error in opening the part wrapper";
+    }
+    
+    /*
+     * Attach the embObjInertials,or embObjIMU, or a remapper listed in the options, to the server device
+     */
+    // Parse from the options the list of devices names to attach
+    std::vector<std::string> deviceNameList;
+    GazeboYarpPlugins::getVectorOfStringFromListInConfig("networks", wrapper_properties, deviceNameList);
+    
+    // Retrieve the device objects from the Handler and build the list of devices to attach
+    ::yarp::dev::PolyDriverList deviceList;
+    std::string driverScopedName;
+    
+    // We could consider that the Multiple Analog Sensors Server can have only one device attached but we choose
+    // not to do that assumption here for reducing the dependencies. If the number of devices to attach exceeds
+    // the wrapper rules, the wrapper itself raises the error.
+    for (size_t nameIdx=0; nameIdx<deviceNameList.size(); nameIdx++)
+    {
+        driverScopedName = m_robotName+"::"+deviceNameList[nameIdx];
+        yarp::dev::PolyDriver* driver2add = GazeboYarpPlugins::Handler::getHandler()->getDevice(driverScopedName);
+        if (!driver2add)
+        {
+            yError() << "GazeboYarpMASwrapper associated driver retrieval from Handler failed.";
+        }
+        deviceList.push(driver2add,deviceNameList[nameIdx].c_str());
+    }
+
+    // Attach the drivers. (If the number of devices to attach exceeds the wrapper rules, the wrapper itself
+    // raises the error.)
+    m_MASwrapperDevice.view(m_iWrap);
+    if(!m_iWrap->attachAll(deviceList) )
+    {
+        yError() << "GazeboYarpMASwrapper : error in connecting wrapper and device ";
+    }
+}
+
+}
