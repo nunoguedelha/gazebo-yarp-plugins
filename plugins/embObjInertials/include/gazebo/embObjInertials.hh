45e3bb68c866f07e24e53dff5155abf91820b339
Nuno Guedelha <nuno.guedelha@iit.it>
2018-07-18 13:47:55 +0200

    Implemented 'embObjInertials' plugin, which wraps the device that emulates the 'embObjInertials' from icub-firmware-shared

---
+++ plugins/embObjInertials/include/gazebo/embObjInertials.hh
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2013-2018 Fondazione Istituto Italiano di Tecnologia RBCS & iCub Facility & ADVR
+ * Authors: see AUTHORS file.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or any later version, see LGPL.TXT or LGPL3.TXT
+ */
+
+
+#ifndef GAZEBOYARP_EMBOBJINERTIALS_HH
+#define GAZEBOYARP_EMBOBJINERTIALS_HH
+
+#include <gazebo/common/Plugin.hh>
+
+#include <yarp/dev/PolyDriver.h>
+
+#include <string>
+
+
+// Forward declarations
+namespace yarp {
+    namespace dev {
+        class GazeboYarpEmbObjInertialsDriver;
+    }
+}
+
+namespace gazebo
+{
+    namespace sensors {
+        class ImuSensor;
+    }
+
+    /// \class GazeboYarpEmbObjInertials
+    /// Gazebo Plugin emulating the "embObjInertials" firmware device in Gazebo.
+    ///
+    /// This plugin instantiates the "embObjInertials" device for the Gazebo simulator
+    /// to expose the inertial sensors connected to a virtual EMS board. The virtual
+    /// EMS board is defined by the configuration file embedded in the URDF model, and
+    /// which parameters are passed to the Model plugin calling the methods of this class.
+    /// Since an EMS board can host a set of sensors distributed accross a full part of
+    /// the robot (e.g. EMS10 in the left leg), the respective configuration file in the
+    /// URDF shall be handled by a model model versus a sensor plugin.
+    ///
+    /// The virtual EMS board can be configurated using the yarpConfigurationFile sdf
+    /// tag, that contains a Gazebo URI pointing at a yarp .ini configuration file
+    /// containing the configuration parameters of the board.
+    ///
+    /// The yarpConfigurationFile is the exact same configuration file used on the real
+    /// robot for the same purposes.
+    ///
+    class GazeboYarpEmbObjInertials : public ModelPlugin
+    {
+    public:
+        GazeboYarpEmbObjInertials();
+        virtual ~GazeboYarpEmbObjInertials();
+        /**
+         * Saves the gazebo pointer, retrieves the configuration parameters from the
+         * sdf file, creates and opens the device driver with those parameters as input
+         * configuration for the driver (enabled connected sensors).
+         */
+        virtual void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+
+    private:
+        yarp::dev::PolyDriver m_embObjInertialsDriver;
+        yarp::dev::GazeboYarpEmbObjInertialsDriver* m_iInertials;
+        std::string m_robotName;
+    };
+}
+
+#endif // GAZEBOYARP_EMBOBJINERTIALS_HH
---
+++ plugins/embObjInertials/src/embObjInertials.cc
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2013-2018 Fondazione Istituto Italiano di Tecnologia RBCS & iCub Facility & ADVR
+ * Authors: see AUTHORS file.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or any later version, see LGPL.TXT or LGPL3.TXT
+ */
+
+#include "embObjInertials.hh"
+#include "embObjInertialsDriver.h"
+
+#include <gazebo/physics/physics.hh>
+#include <GazeboYarpPlugins/Handler.hh>
+#include <GazeboYarpPlugins/common.h>
+#include <GazeboYarpPlugins/ConfHelpers.hh>
+
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Log.h>
+#include <yarp/os/LogStream.h>
+
+GZ_REGISTER_MODEL_PLUGIN(gazebo::GazeboYarpEmbObjInertials)
+
+namespace gazebo {
+
+GazeboYarpEmbObjInertials::GazeboYarpEmbObjInertials() : ModelPlugin()
+{
+}
+
+GazeboYarpEmbObjInertials::~GazeboYarpEmbObjInertials()
+{
+    if(m_embObjInertialsDriver.isValid()) {m_embObjInertialsDriver.close();}
+    yarp::os::Network::fini();
+}
+
+void GazeboYarpEmbObjInertials::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+    yarp::os::Network::init();
+    if (!yarp::os::Network::checkNetwork(GazeboYarpPlugins::yarpNetworkInitializationTimeout))
+    {
+        yError() << "GazeboYarpEmbObjInertials::Load error: yarp network does not seem to be available, is the yarpserver running?";
+        return;
+    }
+
+    if (!_parent)
+    {
+        gzerr << "GazeboYarpEmbObjInertials plugin requires a parent Model." << std::endl;
+        return;
+    }
+
+    // Get the model scoped name
+    m_robotName = _parent->GetScopedName(true);
+
+    // Add my gazebo device driver to the factory.
+    ::yarp::dev::Drivers::factory().add(new ::yarp::dev::DriverCreatorOf< ::yarp::dev::GazeboYarpEmbObjInertialsDriver>
+                                        ("gazebo_embObjInertials", "multipleanalogsensorsserver", "GazeboYarpEmbObjInertialsDriver"));
+
+    // Getting .ini configuration file parameters from sdf
+    ::yarp::os::Property driver_properties;
+
+    bool configuration_loaded = GazeboYarpPlugins::loadConfigModelPlugin(_parent,_sdf,driver_properties);
+
+    if (!configuration_loaded)
+    {
+        yError() << "GazeboYarpEmbObjInertials : File .ini not found, load failed." ;
+        return;
+    }
+
+    // Open the device driver
+    if (!m_embObjInertialsDriver.open(driver_properties))
+    {
+        yError() << "GazeboYarpEmbObjInertials Plugin Load failed: error in opening the part driver";
+    }
+    
+    // Get the driver scoped name "<robotName>::<driverName>". The driver name is unique in the robot URDF model
+    m_embObjInertialsDriver.view(m_iInertials);
+    std::string driverScopedName = m_iInertials->getDriverScopedName();
+    
+    //Insert the pointer in the singleton handler for retriving it in the yarp driver
+    if (!GazeboYarpPlugins::Handler::getHandler()->setDevice(driverScopedName,&m_embObjInertialsDriver))
+    {
+        yError() << "GazeboYarpEmbObjInertials associated driver registering to Handler failed.";
+    }
+}
+
+}
